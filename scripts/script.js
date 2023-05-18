d09bf353bb28feceafe7172487c566ba551a9044
Vitalii Zadniprianets <vzadnepr@ukr.net>
2023-05-18 16:16:38 +0300

    add some function JS

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,19 @@ let firstName = "Vitalii";
 console.log(firstName);
 
 let lastName = "Zadniprianets";
-console.log(lastName);
+console.log(firstName);
+
+let surName = 'Igorovich';
+console.log(surName);
+
+let someString = " ";
+console.log(someString);
+
+function getFullName (first,last,sur,string) {
+  let someFullName = first + string + sur + string + last;
+  console.log(someFullName);
+};
+
+let fullName = "";
+
+fullName = getFullName(firstName,lastName,surName,someString);
